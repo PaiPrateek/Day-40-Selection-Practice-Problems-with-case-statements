2647dfbf26742517445303b11986dd9588cfe447
Prateek <prateekvasanthpai@gmail.com>
2022-07-30 12:30:02 +0530

    Read Numbers 10, 100, 1000 so on

--- SelectionPracticeProblem.js
+++ SelectionPracticeProblem.js
@@ -71,3 +71,36 @@ switch(DayNumber){
         console.log("Invalid number");
         break;
 }
+
+
+//Display Unit Tens thousands so on ....
+let Power = Math.floor(Math.random()*10)%7;
+let Numb = 10**power;
+console.log("Number : "+Numb);
+switch(Numb){
+    case 1:
+        console.log("One");
+        break;
+    case 10:
+        console.log("Ten");
+        break;
+    case 100:
+        console.log("Hundred");
+        break;
+    case 1000:
+        console.log("Thousand");
+        break;
+    case 10000:
+        console.log("Ten thousand");
+        break;
+    case 100000:
+        console.log("One Lakh");
+        break;
+    case 1000000:
+        console.log("Ten Lakh");
+        break;
+    default:
+        console.log("Not in Range");
+        break;
+}
+
